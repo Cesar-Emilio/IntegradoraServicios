79f197d0c929d313f170d5101edf9e4f331ba5a7
Cesar <20233tn076@utez.edu.mx>
2024-11-18 14:57:32 -0600

    Primer avance del día :D

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import ProtectedRoute from "./pages/ProtectedRoute.tsx";
+import { AuthProvider } from "./auth/authProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+    {
+        path: "/",
+        element: <ProtectedRoute />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </StrictMode>
+);
