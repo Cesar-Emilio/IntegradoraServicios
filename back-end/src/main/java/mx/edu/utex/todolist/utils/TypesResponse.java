3e29f5f3f7f685686d14a2b777b1002662684728
Cesar <20233tn076@utez.edu.mx>
2024-11-19 20:30:10 -0600

    Carpeta pal bakend

---
+++ back-end/src/main/java/mx/edu/utex/todolist/utils/TypesResponse.java
@@ -0,0 +1,7 @@
+package mx.edu.utex.todolist.utils;
+
+public enum TypesResponse {
+    SUCCESS,
+    ERROR,
+    WARNING
+}
---
+++ back-end/src/test/java/mx/edu/utex/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utex.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
