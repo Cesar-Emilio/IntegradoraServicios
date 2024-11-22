c9794e9e0ac5f7824cfe2ffdc581bfd51466adc3
Christopher <20233tn091@utez.edu.mx>
2024-11-21 21:03:21 -0600

    Categorias

--- back-end/src/main/java/mx/edu/utex/todolist/category/control/CategoryController.java
+++ back-end/src/main/java/mx/edu/utex/todolist/category/control/CategoryController.java
@@ -1,4 +1,63 @@
 package mx.edu.utex.todolist.category.control;
 
+import mx.edu.utex.todolist.category.model.Category;
+import mx.edu.utex.todolist.utils.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    // Registrar categoría
+    @PostMapping("/register")
+    public ResponseEntity<Message> registerCategory(@RequestBody Category category) {
+        return categoryService.registerCategory(category);
+    }
+
+    // Consultar todas las categorías
+    @GetMapping("/all")
+    public ResponseEntity<Message> findAll() {
+        return categoryService.consultCategories();
+    }
+
+    // Consultar categoría por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Message> findById(@PathVariable Long id) {
+        return categoryService.idCategory(id);
+    }
+
+    // Eliminar categoría
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Message> deleteCategory(@PathVariable Long id) {
+        return categoryService.deleteCategory(id);
+    }
+
+    // Actualizar categoría
+    @PutMapping("/update")
+    public ResponseEntity<Message> updateCategory(@RequestBody Category category) {
+        return categoryService.updateCategory(category);
+    }
+
+    // Activar categoría
+    @PutMapping("/{id}/activate")
+    public ResponseEntity<Message> activateCategory(@PathVariable Long id) {
+        return categoryService.activateCategory(id);
+    }
+
+    // Desactivar categoría
+    @PutMapping("/{id}/deactivate")
+    public ResponseEntity<Message> desactivateCategory(@PathVariable Long id) {
+        return categoryService.desactivateCategory(id);
+    }
+
+    // Añadir nueva categoría
+    @PostMapping("/add")
+    public ResponseEntity<Message> addCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
 }
--- back-end/src/main/java/mx/edu/utex/todolist/category/model/Category.java
+++ back-end/src/main/java/mx/edu/utex/todolist/category/model/Category.java
@@ -1,4 +1,69 @@
 package mx.edu.utex.todolist.category.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(50)")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "VARCHAR(100)")
+    private String description;
+
+    @Column(name = "status", columnDefinition = "BOOL DEFAULT TRUE")
+    private boolean status;
+
+    public Category() {
+    }
+
+    public Category(String name, String description, boolean status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Category(long id, String name, String description, boolean status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
